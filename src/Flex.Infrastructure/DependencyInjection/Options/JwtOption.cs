70c3ab8dc4043050835a5893db9af8479e6172fb
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-06-28 03:24:40 +0700

    Implement module infrastructure

---
+++ src/Flex.Infrastructure/DependencyInjection/Options/JwtOption.cs
@@ -0,0 +1,10 @@
+﻿namespace Flex.Infrastructure.DependencyInjection.Options
+{
+    public class JwtOption
+    {
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string SecretKey { get; set; }
+        public int ExpireMin { get; set; }
+    }
+}
