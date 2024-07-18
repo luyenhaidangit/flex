137b3af1c8b699625f30c7d2e623919e9782cc73
Dang Luyen Hai(LTV-HN) <luyenhaidangit@gmail.com>
2024-07-18 12:38:28 +0700

    Update commit

---
+++ src/Flex.Domain/Common/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flex.Domain.Common.Entities
+{
+    internal class BaseEntity
+    {
+    }
+}
