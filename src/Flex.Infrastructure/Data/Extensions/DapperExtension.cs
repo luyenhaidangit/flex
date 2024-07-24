7cab41489604b5ab7df69a4e9048c738463ee195
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2024-07-24 08:16:01 +0700

    Update commit

---
+++ src/Flex.Infrastructure/Data/Extensions/DapperExtension.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Text.RegularExpressions;
+
+namespace Flex.Infrastructure.Data.Extensions
+{
+    public static class DapperExtension
+    {
+        public static Task<IEnumerable<dynamic>> SearchAndPaginateAsync(this IDbConnection connection,string sql)
+        {
+
+
+            return null;
+        }
+
+        public static string BuildCountQuery(string baseQuery)
+        {
+            var cleanedQuery = Regex.Replace(baseQuery, @"--.*?$|/\*.*?\*/", string.Empty, RegexOptions.Singleline | RegexOptions.Multiline).Trim();
+
+            // Tìm vị trí của từ khóa FROM sử dụng biểu thức chính quy
+            var fromRegex = new Regex(@"\bFROM\b", RegexOptions.IgnoreCase);
+            var match = fromRegex.Match(cleanedQuery);
+
+            if (!match.Success)
+            {
+                throw new ArgumentException("Invalid query sql!");
+            }
+
+            // Xây dựng câu lệnh COUNT
+            string countQuery = $"SELECT COUNT(*) {cleanedQuery.Substring(match.Index)}";
+            return countQuery;
+        }
+    }
+}
